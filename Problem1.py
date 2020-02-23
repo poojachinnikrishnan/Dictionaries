ca83d5099cf358659f64a558463d076aa83f4718
poojachinnikrishnan <60472985+poojachinnikrishnan@users.noreply.github.com>
2020-02-23 14:01:06 +0530

    Code for calculating protein mass

---
+++ Problem1.py
@@ -0,0 +1,8 @@
+protein = input("ENTER THE SEQUENCE:")
+molecular_mass={"A": 71.07, "R":156.18, "N":114.08, "D":115.08, "C":103.10, "Q":128.13,"E":129.11, "G": 57.05, "H":137.14, "I":113.15, "L":113.15, "K":128.17, "M":131.19, "F":147.17,"P": 97.11, "S": 87.07, "T":101.10, "W":186.20, "Y":163.17, "V": 99.13}
+Mass=0
+for i in protein:
+	Mass = Mass + molecular_mass[i]
+
+Mass= Mass+18.02
+print(Mass)
